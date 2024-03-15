0cf01c3a74586a2de5511bce07955fdd5d50ce85
Moriooo-625 <ttt_mt_625@yahoo.co.jp>
2024-03-15 22:27:28 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Hello Tomonori Moriooo</h1>
+      <h1>Vite + React Hello Tomonori Moriooo-625</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
